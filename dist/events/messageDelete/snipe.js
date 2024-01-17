f95180747f467c697ac5768110ef2859bb5bc2d4
Rena Official <128772893+RenaOfficial@users.noreply.github.com>
2024-01-18 01:30:09 +0900

    Delete dist directory

--- dist/events/messageDelete/snipe.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Event_1 = require("../../lib/classes/Event");
-const index_1 = require("../../index");
-exports.default = new Event_1.Event("messageDelete", async (message) => {
-  if (!message.content && !message.attachments) return;
-  index_1.client.snipes.set(message.channel.id, message);
-});
